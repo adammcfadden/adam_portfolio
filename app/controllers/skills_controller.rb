596c22997d1329c6f50feb191929b5b15d04fdec
Adam McFadden <mcfadden.113@gmail.com>
2015-07-02 11:16:17 -0700

    add ability to create new skills, new skills are placed on skills index. an integration spec is testing this and passing.

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -4,26 +4,38 @@ def index
   @skills = Skill.all
 end
 
-def show
-
-end
-
+# def show
+#
+# end
+#
 def new
-
+  @skill = Skill.new
 end
 
 def create
-
+  @skill = Skill.new(skill_params)
+  if @skill.save
+    flash[:notice] = "Skill Added"
+    redirect_to root_path
+  else
+    render :new
+  end
 end
 
-def edit
+# def edit
+#
+# end
+#
+# def update
+#
+# end
+#
+# def destroy
+#
+# end
 
+private def skill_params
+  params.require(:skill).permit(:name, :description)
 end
-
-def update
-
-end
-
-def destroy
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'skills#index'
+
   resources :skills
+
 end
